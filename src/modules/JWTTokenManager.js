b32989645f0a44b8dd1e789899eafb40186ad686
Tomasz Pecak <pecatoma@gmail.com>
2019-09-06 11:47:18 +0200

    Add JWTTokenManger

---
+++ src/modules/JWTTokenManager.js
@@ -0,0 +1,14 @@
+import * as SecureStore from 'expo-secure-store';
+const TOKEN = 'token'
+
+export const saveToken = async (token) => {
+    await SecureStore.setItemAsync(TOKEN, token)
+};
+
+export const getToken = async () => {
+    return await SecureStore.getItemAsync(TOKEN)
+};
+
+export const removeToken = async () => {
+    await SecureStore.deleteItemAsync(TOKEN)
+};
