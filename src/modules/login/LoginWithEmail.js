96ef6fdc71ebe0a6abf9330daf781a20ba3adf5a
Tomasz Pecak <pecatoma@gmail.com>
2020-02-21 10:57:58 +0100

    Fix eslint errors

--- src/modules/login/LoginWithEmail.js
+++ src/modules/login/LoginWithEmail.js
@@ -1,20 +1,21 @@
-import config from '../../../config/config';
 import axios from 'axios';
+import config from '../../../config/config';
 
-export default async (email, pair_nr) => {
+export default async (email, pairNr) => {
   try {
     const accessUrl = [config.baseUrl, 'api/v1/login'].join('');
     const response = await axios.post(accessUrl, {
       email,
-      pair_nr,
+      pair_nr: pairNr,
     });
-    //TODO: Check response status and uncomment console error
+    // TODO: Check response status and uncomment console error
     if (response.status === 200) {
-      console.log('RESPONSE DATA ' + response.data);
+      console.log(`RESPONSE DATA ${response.data}`);
       return response.data;
     }
+    return null;
   } catch (e) {
-    //console.error(e);
+    // console.error(e);
     return { error: e };
   }
 };
