043924faaa62f05dff9ae172eba403f8b5be6971
Tomasz Pecak <pecatoma@gmail.com>
2020-03-06 01:09:52 +0100

    Add google maps api key, install necessary build packages

--- config/config.js
+++ config/config.js
@@ -1,11 +1,5 @@
-import {
-  ANDROID_CLIENT_ID,
-  IOS_CLIENT_ID,
-  BASE_URL,
-} from 'react-native-dotenv';
+import { BASE_URL } from 'react-native-dotenv';
 
 module.exports = {
-  iosClientId: IOS_CLIENT_ID,
-  androidClientId: ANDROID_CLIENT_ID,
   baseUrl: BASE_URL,
 };
