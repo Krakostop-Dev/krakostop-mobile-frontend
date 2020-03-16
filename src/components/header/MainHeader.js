eb73acc1bd7cd7617e0f7832285b0ad111e2b341
Tomasz Pecak <pecatoma@gmail.com>
2020-03-16 12:01:44 +0100

    Add basic search bar

--- src/components/header/MainHeader.js
+++ src/components/header/MainHeader.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { Header } from 'react-native-elements';
 import { ksStyle } from '../../styles/basic/ksBasic';
 import HeaderLeft from './HeaderLeft';
-import HeaderCenter from './HeaderCenter';
 import HeaderRight from './HeaderRight';
+import CustomSearchBar from './searchBar';
 
 const MainHeader = () => {
   return (
     <Header
       leftComponent={HeaderLeft}
-      centerComponent={HeaderCenter}
       rightComponent={HeaderRight}
       containerStyle={{
         backgroundColor: ksStyle.colors.primaryColorDark,
