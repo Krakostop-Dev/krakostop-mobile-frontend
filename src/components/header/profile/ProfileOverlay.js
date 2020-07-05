354e3e05b52881fa1a6a0082a6a961e242c8fb4d
Aneta <anpociecha@gmail.com>
2020-07-05 18:36:49 +0200

    add logout button in profile view

--- src/components/header/profile/ProfileOverlay.js
+++ src/components/header/profile/ProfileOverlay.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import KSOverlay from '../../ksOverlay/KSOverlay';
 import KSOverlayHeader from '../../ksOverlay/KSOverlayHeader';
 import CardContent from '../../card/CardContent';
 import ProfileViewContentHeader from '../../../screens/profile/ProfileViewContentHeader';
 import ProfileViewContentForm from '../../../screens/profile/ProfileViewContentForm';
+import LogoutView from './LogoutView';
+
+const styles = StyleSheet.create({
+  card: { paddingHorizontal: 0 },
+});
 
 function ProfileOverlay({ setIsVisible, isVisible }) {
   return (
@@ -14,10 +20,14 @@ function ProfileOverlay({ setIsVisible, isVisible }) {
         title="Profil"
         setIsVisible={setIsVisible}
       />
-      <CardContent>
-        <ProfileViewContentHeader />
-        <ProfileViewContentForm />
-      </CardContent>
+
+      <ScrollView>
+        <CardContent style={styles.card}>
+          <ProfileViewContentHeader />
+          <ProfileViewContentForm />
+          <LogoutView />
+        </CardContent>
+      </ScrollView>
     </KSOverlay>
   );
 }
