1365c099c4aeec7d94b6f4557fc403448aebebb6
Tomasz Pecak <pecatoma@gmail.com>
2020-07-21 19:11:44 +0200

    Create safe area component to get rid of header overlapping

---
+++ src/styles/SafeArea.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+import { ksStyle } from './basic/ksBasic';
+
+const styles = StyleSheet.create({
+  androidSafeArea: {
+    flex: 1,
+    backgroundColor: ksStyle.colors.primaryColorDark,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+});
+
+const SafeArea = ({ children }) => (
+  <SafeAreaView style={styles.androidSafeArea}>{children}</SafeAreaView>
+);
+
+export default SafeArea;
+
+SafeArea.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
